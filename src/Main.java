b1b7fc0e02026b020806eee52796af13c24a050c
kufre.akpabio <akpabiosun@gmail.com>
2024-07-12 15:23:40 +0100

    new code added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws InterruptedException {
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Batch 19 is great");
+            Thread.sleep(2000);
         }
+
     }
+}
